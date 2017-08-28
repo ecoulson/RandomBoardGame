6f8f6d71476ff11bdbd8d7972f715082f926c42f
ecoulson <ivansonofcoul@gmail.com>
2017-08-28 00:05:39 -0700

    work on player movement

--- app/src/index.js
+++ app/src/index.js
@@ -29,6 +29,8 @@ let game = (function() {
     getPlayer = getPlayer.bind(this);
     getPlayers = getPlayers.bind(this);
     render = render.bind(this);
+    levelUp = levelUp.bind(this);
+    movePlayer = movePlayer.bind(this);
 
     const playerGeometry = new THREE.ConeGeometry(1.5, 4.25, 32);
     const boardGeometry = new THREE.BoxGeometry(100, 130, 0.1);
@@ -39,30 +41,56 @@ let game = (function() {
 
 
     this.state = {
-        player: {
-            name: 'test',
+        player: null,
+        playerMeshes: {
+
         },
     };
     this.state.players = [this.state.player];
     
-    socket.emit('getState', createPayload(this.state.player))
+    socket.emit('getState', createPayload({ name: 'test' }));
     
     socket.on('updatePlayers', (payload) => {
         let json = handlePayload(payload);
+        if (this.state.player === null) {
+            this.state.player = json[json.length - 1];
+            createPlayerMesh(this.state.player);
+        } else {
+            for (let i = 0; i < json.length; i++) {
+                if (json[i].id == this.state.player.id) {
+                    this.state.player = json[i];
+                }
+            }
+        }
+        if (this.state.players.length < json.length) {
+            for (let i = this.state.players.length - 1; i < json.length; i++) {
+                createPlayerMesh(json[i]);
+            }
+        }
         this.state.players = json;
-        this.state.player = this.state.players[json.length - 1];
-        createPlayerMeshes();
     });
 
     window.addEventListener('resize', onWindowResize, false);
 
     function start() {
+        setup();
+        
+        addLights();
+
+        createBoardMesh();
+
+        render();
+    }
+
+    function setup() {
         renderer.shadowMap.enabled = true;
         renderer.shadowMap.type = THREE.PCFSoftShadowMap;
         renderer.setSize( window.innerWidth, window.innerHeight );
         scene.background = new THREE.Color( 0x727882 );
         document.body.appendChild(renderer.domElement);
-        
+    }
+
+    function addLights() {
         var light2 = new THREE.DirectionalLight(0xffffff, 0.9, 100);
         light2.position.set(-100, -100, 150);
         light2.castShadow = true;
@@ -82,27 +110,23 @@ let game = (function() {
 
         var ambientLight = new THREE.AmbientLight( 0x151515 ); // soft white light
         scene.add( ambientLight );
+    }
 
+    function createBoardMesh() {
         let boardMesh = new THREE.Mesh(boardGeometry, boardMaterial);
         boardMesh.receiveShadow = true;
         scene.add(boardMesh);
-
-        render();
     }
 
-    function createPlayerMeshes() {
-        this.state.playerMeshes = [];
-        this.state.players.forEach((player) => {
-            let playerMesh = new THREE.Mesh(playerGeometry, playerMaterial);
-            this.state.playerMeshes.push(playerMesh);
-            playerMesh.rotation.x = 90 * Math.PI / 180;
-            player.mesh = playerMesh;
-            playerMesh.position.x = player.pos.x;
-            playerMesh.position.y = player.pos.y;
-            playerMesh.position.z = player.pos.z;
-            playerMesh.castShadow = true;
-            scene.add(playerMesh);
-        })
+    function createPlayerMesh(player) {
+        let playerMesh = new THREE.Mesh(playerGeometry, playerMaterial);
+        playerMesh.rotation.x = 90 * Math.PI / 180;
+        playerMesh.position.x = player.pos.x;
+        playerMesh.position.y = player.pos.y;
+        playerMesh.position.z = player.pos.z;
+        playerMesh.castShadow = true;
+        this.state.playerMeshes[player.id] = playerMesh;
+        scene.add(playerMesh);
     }
 
     function getPlayer() {
@@ -114,8 +138,26 @@ let game = (function() {
     }
 
     function render() {
+        if (this.state.player !== null) {
+            movePlayer();
+        }
         renderer.render(scene, camera);
         requestAnimationFrame(render);
+    }
+
+    function levelUp(l) {
+        this.state.player.level += l;
+        console.log(createPayload(this.state.player));
+        socket.emit('p-level', createPayload(this.state.player));
+    }
+
+    function movePlayer() {
+        this.state.players.forEach((player) => {
+            let mesh = this.state.playerMeshes[player.id];
+            mesh.position.x = player.pos.x;
+            mesh.position.y = player.pos.y;
+            mesh.position.z = player.pos.z;
+        })
     }
 
     function onWindowResize() {
@@ -126,8 +168,10 @@ let game = (function() {
 
     return {
         player: getPlayer,
+        meshs: this.state.playerMeshes,
         players: getPlayers,
         start: start,
+        levelUp: levelUp,
     };
 })();
 game.start();
